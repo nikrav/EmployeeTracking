278c83caeb5502e53ebd2a7d84a0fb5124b7014d
ddhormuth0 <dawsonhormuth@gmail.com>
2023-01-19 21:24:38 -0500

    Add Input to MySQL Server From React

--- backend/components/routes.js
+++ backend/components/routes.js
@@ -7,15 +7,42 @@ const pool = require('./db.js');
 //enter routes here
 router.get("/employees", async (req, res) => {
     console.log("Request Made");
-    pool.getConnection( (err, con) => {
-        if(err) throw err; //not connected
+    pool.getConnection((err, conn) => {
+        if (err) throw err; //not connected
         //query the database
-        con.query('SELECT * FROM employees', function (error, result, fields){
+        conn.query('SELECT * FROM employees', function (error, result, fields) {
             //send the result in a json
-            res.json(result)
+            conn.release();
+            if(error) throw error;
+            res.json(result);
         })
     })
 })
+
+router.post("/add-employee", (req, res) => {
+    //get the information that we want to add
+    console.log("Requested to Add Employee");
+    const fName = req.body.fName;
+    const lName = req.body.lName;
+
+    //connect to the database
+    pool.getConnection((err, conn) => {
+        if (err) throw err;
+
+        //set up the string, the ? ? represent variables that we will imput later
+        const insertQry = 'INSERT INTO employees (fName, lName) VALUES (?, ?);';
+        //run the insert command
+        conn.query(insertQry, [fName, lName], (error, result) => {
+            conn.release();
+            if(error) throw error;
+            console.log("Employee Added");
+        })
+    })
+
+
+})
+
+
 
 //exports our routes
 module.exports = router;
--- frontend/src/components/Employees.jsx
+++ frontend/src/components/Employees.jsx
@@ -1,29 +1,61 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-function Employees(){
+function Employees() {
 
     const [employees, setEmployees] = useState([]);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
 
     //the information we are getting from the backend
     useEffect(() => {
         //get request
         fetch(process.env.REACT_APP_PROXY + "/employees")
-        //turns data into json
-        .then(res => res.json())
-        //sets the employee data to the data we can use in the state
-        .then(employeesData => setEmployees(employeesData))
-        //catches errors
-        .catch(err => console.log(err));
+            //turns data into json
+            .then(res => res.json())
+            //sets the employee data to the data we can use in the state
+            .then(employeesData => setEmployees(employeesData))
+            //catches errors
+            .catch(err => console.log(err));
     }, []);
     // [] at the end determines when to make the request, [ ] by itself only makes the request once
 
+    //handles the submit request by sending a post request
+    function handleSubmit(evt){
+        //stops page from refreshing after submit
+        // evt.preventDefault();
+        //object we are going to send/post
+        const employee = {
+            fName: firstName, 
+            lName: lastName
+        }
+        console.log(employee);
+
+        //sends the post request
+        fetch(process.env.REACT_APP_PROXY + "/add-employee", {
+            //type of method we are doing
+            method: "POST",
+            //type of information we are sending
+            headers: {  "Content-Type": "application/json" },
+            //data we are sending
+            body: JSON.stringify(employee)
+        }).then(()=>{
+            console.log("New Employee Added");
+        })
+    }
 
     //returns our data in html
-    return(
+    return (
         <div>
-        {employees.map(employee =>(
-            <h1 key={employee.employee_id}>{employee.fName}</h1>
-        ))}
+            {employees.map(employee => (
+                <h1 key={employee.employee_id}>{employee.fName}</h1>
+            ))}
+            <form onSubmit={handleSubmit}>
+                <label>Enter First Name</label>
+                <input type="text" name="fName" required onChange={(change) => setFirstName(change.target.value)}/>
+                <label>Enter Last Name</label>
+                <input type="text" name="lName" required onChange={(change) => setLastName(change.target.value)}/>
+                <button type="submit">Add Employee</button>
+            </form>
         </div>
     );
 }
