0bd5a0c6f6212987329f14d3054c82f89f7909f7
ddhormuth0 <dawsonhormuth@gmail.com>
2023-01-26 12:28:29 -0500

    Add Journal Submission Error Checking

--- frontend/src/components/Journals/WriteJournals.jsx
+++ frontend/src/components/Journals/WriteJournals.jsx
@@ -17,26 +17,36 @@ function WriteJournals(props) {
 
         //stops page from refreshing after submit
         evt.preventDefault();
-        //object we are going to send/post
-        const journal = {
-            receivingID: employeeID,
-            journalTypeInfo: journalType,
-            journalDate: newJournalDate,
-            content: info,
-            givingID: employeeIDGiving
-        }
 
-        console.log(employeeID + " " + employeeIDGiving)
-        fetch(process.env.REACT_APP_PROXY + "/journals", {
-            //type of method we are doing
-            method: "POST",
-            //type of information we are sending
-            headers: { "Content-Type": "application/json" },
-            //data we are sending
-            body: JSON.stringify(journal)
-            //if props state is true then we set it to false, and vice versa, this will reload the journals
-        }).then(() => { props.changeState(props.state ? false : true); })
-            .catch(err => console.log(err));
+
+        //if nothing is udefined do the submission
+        if (info === "" || employeeIDGiving === "" || journalType === "Type" || employeeID === "" || newJournalDate === "") {
+            alert("Please Fill Out the Entire Journal Form")
+        }
+        else {
+
+            //object we are going to send/post
+            const journal = {
+                receivingID: employeeID,
+                journalTypeInfo: journalType,
+                journalDate: newJournalDate,
+                content: info,
+                givingID: employeeIDGiving
+            }
+
+            console.log(journal);
+
+            fetch(process.env.REACT_APP_PROXY + "/journals", {
+                //type of method we are doing
+                method: "POST",
+                //type of information we are sending
+                headers: { "Content-Type": "application/json" },
+                //data we are sending
+                body: JSON.stringify(journal)
+                //if props state is true then we set it to false, and vice versa, this will reload the journals
+            }).then(() => { props.changeState(props.state ? false : true); })
+                .catch(err => console.log(err));
+        }
 
     }
 
@@ -45,25 +55,25 @@ function WriteJournals(props) {
 
         <div className="row px-4 my-5 position-relative">
             {/* centers the journals */}
-            <div className="col-md-4"/>
+            <div className="col-md-4" />
             <div className="card col-12 col-md-4 my-2 p-0">
                 <div className={"card-header " + cardColor}>
                     <button className="btn btn-secondary dropdown-toggle ms-0 mb-0 d-inline-block" type="button" data-bs-toggle="dropdown" aria-expanded="false">{journalType}</button>
-                    <button type="button" className="btn btn-secondary" style = {{position: "relative", float:"right"}} onClick={submitJournal}>Add Journal</button>
+                    <button type="button" className="btn btn-secondary" style={{ position: "relative", float: "right" }} onClick={submitJournal}>Add Journal</button>
                     <ul className="dropdown-menu">
                         {/*When a button is clicked, it changes the orderBy state so that the order can be changed in the get request */}
-                        <li><button onClick={() => {setJournalType("Good"); setCardColor("text-bg-primary")}} className="dropdown-item">Good</button></li>
-                        <li><button onClick={() => {setJournalType("Info"); setCardColor("text-bg-white")}} className="dropdown-item">Info</button></li>
-                        <li><button onClick={() => {setJournalType("Bad"); setCardColor("text-bg-danger")}} className="dropdown-item">Bad</button></li>
+                        <li><button onClick={() => { setJournalType("Good"); setCardColor("text-bg-primary") }} className="dropdown-item">Good</button></li>
+                        <li><button onClick={() => { setJournalType("Info"); setCardColor("text-bg-white") }} className="dropdown-item">Info</button></li>
+                        <li><button onClick={() => { setJournalType("Bad"); setCardColor("text-bg-danger") }} className="dropdown-item">Bad</button></li>
                     </ul>
                 </div>
                 <div className="card-body search-bar-dropdown">
-                    <SearchEmployees setID={setEmployeeID} setShow={setShowInfo} show={showInfo} onClickOutside={() => {setShowInfo(false)}}/>
-                    <textarea className="mt-3 mb-0 p-1 border border-3 text-box" onChange={(e)=>setInfo(e.target.value)}></textarea>
+                    <SearchEmployees setID={setEmployeeID} setShow={setShowInfo} show={showInfo} onClickOutside={() => { setShowInfo(false) }} />
+                    <textarea className="mt-3 mb-0 p-1 border border-3 text-box" onChange={(e) => setInfo(e.target.value)}></textarea>
                 </div>
                 <div className="card-footer">
-                    <input type="date" className="d-inline-block mb-0 ms-0" onChange={(e)=>setNewJournalDate(e.target.value)}></input>
-                    <SearchEmployees classItem={{position: "relative", float:"right"}} setID={setEmployeeIDGiving} setShow={setShowInfoGiving} show={showInfoGiving} onClickOutside={() => {setShowInfoGiving(false)}}/>
+                    <input type="date" className="d-inline-block mb-0 ms-0" onChange={(e) => setNewJournalDate(e.target.value)}></input>
+                    <SearchEmployees classItem={{ position: "relative", float: "right" }} setID={setEmployeeIDGiving} setShow={setShowInfoGiving} show={showInfoGiving} onClickOutside={() => { setShowInfoGiving(false) }} />
                 </div>
             </div>
         </div>
